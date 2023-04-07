f7c3ebba54aeed524b28adcc1e7064dedb4dc749
mobleyj2 <122937476+mobleyj2@users.noreply.github.com>
2023-04-07 12:09:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
 });
 
-//add time blocks, in one hour increments from 9am to 5pm, each block has a save button on the right hand side that saves the info entered to local storage
 
-//add current day at the top of the planner (moment)
+
+
 var currentTime = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentTime);
 
-//variables for now (as a baseline time, use moment) and HTML time block (to change color relative to now)
+
 var timeBlock = $(".hour");
 var now = parseInt(moment().format("H"));
 
@@ -41,10 +22,11 @@ $.each(timeBlock, function (i, hour) {
   } else if (hourId > now) {
     $(this).next().addClass("future");
   }
+  
 });
 
-
 $(".saveBtn").on("click", function (event) {
+
   var calendarItem =
     event.target.parentElement.previousElementSibling.children[0].value;
   localStorage.setItem(event.target.attributes[0].value, calendarItem);
@@ -105,4 +87,4 @@ $(document).ready(function () {
   } else {
     ("");
   }
-});
+});})
