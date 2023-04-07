0ad021b43bece2d14f963649e0a8de9eed85c525
mobleyj2 <122937476+mobleyj2@users.noreply.github.com>
2023-04-06 22:35:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+});
+
+//add time blocks, in one hour increments from 9am to 5pm, each block has a save button on the right hand side that saves the info entered to local storage
+
+//add current day at the top of the planner (moment)
+var currentTime = moment().format("MMMM Do YYYY");
+$("#currentDay").text(currentTime);
+
+//variables for now (as a baseline time, use moment) and HTML time block (to change color relative to now)
+var timeBlock = $(".hour");
+var now = parseInt(moment().format("H"));
+
+$.each(timeBlock, function (i, hour) {
+  var hourId = parseInt($(this).attr("id"));
+  if (hourId === now) {
+    $(this).next().addClass("present");
+  } else if (hourId < now) {
+    $(this).next().addClass("past");
+  } else if (hourId > now) {
+    $(this).next().addClass("future");
+  }
+});
+
+
+$(".saveBtn").on("click", function (event) {
+  var calendarItem =
+    event.target.parentElement.previousElementSibling.children[0].value;
+  localStorage.setItem(event.target.attributes[0].value, calendarItem);
+});
+
+$(document).ready(function () {
+  if (localStorage["9am"] !== null && localStorage["9am"] !== undefined) {
+    var nineAm = $("<p>" + localStorage["9am"] + "</p>");
+    $("#nineAm").append(nineAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["10am"] !== null && localStorage["10am"] !== undefined) {
+    var tenAm = $("<p>" + localStorage["10am"] + "</p>");
+    $("#tenAm").append(tenAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["11am"] !== null && localStorage["11am"] !== undefined) {
+    var elevenAm = $("<p>" + localStorage["11am"] + "</p>");
+    $("#elevenAm").append(elevenAm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["12pm"] !== null && localStorage["12pm"] !== undefined) {
+    var twelvePm = $("<p>" + localStorage["12pm"] + "</p>");
+    $("#twelvePm").append(twelvePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["1pm"] !== null && localStorage["1pm"] !== undefined) {
+    var onePm = $("<p>" + localStorage["1pm"] + "</p>");
+    $("#onePm").append(onePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["2pm"] !== null && localStorage["2pm"] !== undefined) {
+    var twoPm = $("<p>" + localStorage["2pm"] + "</p>");
+    $("#twoPm").append(twoPm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["3pm"] !== null && localStorage["3pm"] !== undefined) {
+    var threePm = $("<p>" + localStorage["3pm"] + "</p>");
+    $("#threePm").append(threePm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["4pm"] !== null && localStorage["4pm"] !== undefined) {
+    var fourPm = $("<p>" + localStorage["4pm"] + "</p>");
+    $("#fourPm").append(fourPm[0].innerText);
+  } else {
+    ("");
+  }
+  if (localStorage["5pm"] !== null && localStorage["5pm"] !== undefined) {
+    var fivePm = $("<p>" + localStorage["5pm"] + "</p>");
+    $("#fivePm").append(fivePm[0].innerText);
+  } else {
+    ("");
+  }
+});
